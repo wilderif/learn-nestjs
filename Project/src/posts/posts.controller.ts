9936e03507258fbf5d58da11ea2f9df4e5e1b3c2
wilderif <woojuro3@gmail.com>
2024-08-27 01:34:04 +0900

    feat: Implement routes for GET methods
    - Added ‘GET /posts’ to fetch all posts
    - Added ‘GET /posts/:id’ to fetch a post by its ID with error handling for non-existent posts

--- Project/src/posts/posts.controller.ts
+++ Project/src/posts/posts.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from "@nestjs/common";
 import { PostsService } from "./posts.service";
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,18 +16,64 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: "author 1",
+    title: "title 1",
+    content: "content 1",
+    likeCount: 1,
+    commentCount: 1,
+  },
+  {
+    id: 2,
+    author: "author 2",
+    title: "title 2",
+    content: "content 2",
+    likeCount: 2,
+    commentCount: 2,
+  },
+  {
+    id: 3,
+    author: "author 3",
+    title: "title 3",
+    content: "content 3",
+    likeCount: 3,
+    commentCount: 3,
+  },
+];
+
 @Controller("posts")
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  // 1) GET /posts
+  //    모든 post를 다 가져온다.
   @Get()
-  getPost(): Post {
-    return {
-      author: "Test",
-      title: "Test Title",
-      content: "Test Content",
-      likeCount: 1,
-      commentCount: 2,
-    };
+  getPosts() {
+    return posts;
   }
+
+  // 2) GET /posts/:id
+  //    id에 해당되는 post를 가져온다.
+  @Get(":id")
+  getPost(@Param("id") id: string) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+
+  // 3) POST /posts
+  //    Post를 생성한다.
+  @Post()
+  postPost() {}
+
+  // 4) PUT /posts/:id
+  //    id에 해당되는 post를 변경한다.
+
+  // 5) DELETE /posts/:id
+  //    id에 해당되는 psot를 삭제한다.
 }
