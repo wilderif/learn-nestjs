12607adae3f61a29a60cd7d3b8ec6e49e294fec5
wilderif <woojuro3@gmail.com>
2024-08-27 02:04:35 +0900

    feat: Implement ‘POST /posts’ endpoint for creating new posts
    - Added a new POST route to create a new post with author, title, and content
    - The new post is appended to the posts array and returned in the response

--- Project/src/posts/posts.controller.ts
+++ Project/src/posts/posts.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Get,
   NotFoundException,
@@ -69,7 +70,24 @@ export class PostsController {
   // 3) POST /posts
   //    Post를 생성한다.
   @Post()
-  postPost() {}
+  postPosts(
+    @Body("author") author: string,
+    @Body("title") title: string,
+    @Body("content") content: string,
+  ) {
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+
+    posts = [...posts, post];
+
+    return post;
+  }
 
   // 4) PUT /posts/:id
   //    id에 해당되는 post를 변경한다.
