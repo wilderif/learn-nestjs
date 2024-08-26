0727017703ec9b60d9738f7bc2e08982c6666ead
wilderif <woojuro3@gmail.com>
2024-08-26 17:38:43 +0900

    feat: Learn how to return JSON data for GET requests
    - Studied how to configure paths in Controller and Get decorators
    - Implemented method to return JSON data in response to GET requests

--- Project/src/app.controller.ts
+++ Project/src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { get } from "http";
 
-@Controller()
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+@Controller("post")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("post")
+  getPost(): Post {
+    return {
+      author: "Test",
+      title: "Test Title",
+      content: "Test Content",
+      likeCount: 1,
+      commentCount: 2,
+    };
   }
 }
