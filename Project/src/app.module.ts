b42e13e9e563c4334389eb070f38dfb89d810448
wilderif <woojuro3@gmail.com>
2024-08-27 15:47:52 +0900

    chore: Add '@nestjs/config' dependency for using  enviroment variables

--- Project/src/app.module.ts
+++ Project/src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PostsModule } from './posts/posts.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { PostsModule } from "./posts/posts.module";
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    ConfigModule.forRoot({}),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "127.0.0.1",
+      port: 5432,
+      username: "",
+      password: "",
+      database: "",
+    }),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
