b140067e78f89b3ded01d1d9e700c8619bf4970c
wilderif <woojuro3@gmail.com>
2024-08-28 14:59:47 +0900

    feat(TypeORM): Integrate ConfigModule and ConfigService for TypeORM configuration
    - Used ConfigModule and ConfigService to load environment variables from .env file.
    - Applied environment variables to TypeOrmModule configuration.

--- TypeORM/src/app.module.ts
+++ TypeORM/src/app.module.ts
@@ -1,9 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: "127.0.0.1",
+        port: 5432,
+        username: configService.get("POSTGRES_USER"),
+        password: configService.get("POSTGRES_PASSWORD"),
+        database: configService.get("POSTGRES_DB"),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
