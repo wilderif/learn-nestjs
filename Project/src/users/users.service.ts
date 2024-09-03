26eb9d8f00fa105059d88be99b8ff592641b6b3f
wilderif <woojuro3@gmail.com>
2024-09-04 01:09:46 +0900

    feat: Implement user creation and retrieval in UsersService and UsersController
    - Injected UsersModel dependency into UsersService
    - Implemented createUser method in UsersService to save a new user to the database
    - Added POST request in UsersController to create a user using createUser method
    - Implemented getAllUsers method in UsersService and added corresponding GET request in UsersController

--- Project/src/users/users.service.ts
+++ Project/src/users/users.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { UsersModel } from "./entities/users.entity";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersModel)
+    private readonly usersRepository: Repository<UsersModel>,
+  ) {}
+
+  async createUser(nickname: string, email: string, password: string) {
+    const user = this.usersRepository.create({
+      nickname,
+      email,
+      password,
+    });
+
+    const newUser = await this.usersRepository.save(user);
+
+    return newUser;
+  }
+
+  async getAllUsers() {
+    const users = await this.usersRepository.find();
+
+    return users;
+  }
+}
