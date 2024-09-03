b06ad5332bb20f95423b2571cbdfe4c8394d7331
wilderif <woojuro3@gmail.com>
2024-09-04 00:49:33 +0900

    feat: Implement RolesEnum and add options to UserModel columns
    - Added options to columns in UsersModel for better schema definition
    - Defined RolesEnum and applied it to the role column in UsersModel

--- Project/src/users/entities/users.entity.ts
+++ Project/src/users/entities/users.entity.ts
@@ -1,16 +1,28 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { RolesEnum } from "../const/roles.const";
 
 @Entity()
 export class UsersModel {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({
+    length: 20,
+    unique: true,
+  })
   nickname: string;
 
-  @Column()
+  @Column({
+    unique: true,
+  })
   email: string;
 
   @Column()
   password: string;
+
+  @Column({
+    enum: Object.values(RolesEnum),
+    default: RolesEnum.USER,
+  })
+  role: RolesEnum;
 }
