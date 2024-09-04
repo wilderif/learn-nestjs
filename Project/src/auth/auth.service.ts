262f214feea1818bf3f405df34bc24403ddfa5f0
wilderif <woojuro3@gmail.com>
2024-09-04 12:49:01 +0900

    feat: Implement signToken method in AuthService
    - Imported JwtModule and injected JwtService into AuthService
    - Implemented signToken method in AuthService
    - Created JWT payload and included secret key and expiresIn, returning the signed token

--- Project/src/auth/auth.service.ts
+++ Project/src/auth/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { UsersModel } from "src/users/entities/users.entity";
+import { JWT_SECRET } from "./const/auth.const";
 
 @Injectable()
 export class AuthService {
@@ -25,4 +28,26 @@ export class AuthService {
    *      3. 사용자가 존재하고 비밀번호가 일치하면 사용자 정보를 반환
    *     (4. loginWithEmail에서 반환된 데이터를 기반으로 토큰 생성)
    */
+
+  constructor(private readonly jwtService: JwtService) {}
+
+  /**
+   * Payload에 들어갈 정보
+   *
+   * 1) email
+   * 2) sub (id)
+   * 3) type : 'access' : 'refresh'
+   */
+  signToken(user: Pick<UsersModel, "email" | "id">, isRefreshToken: boolean) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? "refresh" : "access",
+    };
+
+    return this.jwtService.sign(payload, {
+      secret: JWT_SECRET,
+      expiresIn: isRefreshToken ? 3600 : 300,
+    });
+  }
 }
