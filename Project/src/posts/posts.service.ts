9b62a3b7ecc02ca321cc355b209f05794b9ca958
wilderif <woojuro3@gmail.com>
2024-09-25 14:49:19 +0900

    feat: Upgrade paginatePosts method with cursor-based pagination
    - Implemented paginatePosts method in PostsService to fetch posts based on PaginatePostDto options.
    - Added logic to generate next URL based on the last post's ID.
    - Included cursor-based pagination by appending 'where__id_more_than' parameter for the next request.
    - Returned pagination details such as data, cursor, count, and next URL in the response.

--- Project/src/posts/posts.service.ts
+++ Project/src/posts/posts.service.ts
@@ -5,6 +5,7 @@ import { PostsModel } from "./entities/posts.entity";
 import { CreatePostDto } from "./dto/create-post.dto";
 import { UpdatePostDto } from "./dto/update-post.dto";
 import { PaginatePostDto } from "./dto/paginate-post.dto";
+import { HOST, PROTOCOL } from "src/common/const/env.const";
 
 @Injectable()
 export class PostsService {
@@ -28,6 +29,16 @@ export class PostsService {
     }
   }
 
+  /**
+   * Response
+   *
+   * data: Data[],
+   * cursor: {
+   *  after: 마지막 data의 ID,
+   * },
+   * count: 응답한 data의 개수,
+   * next: 다음 요청을 할 때 사용할 URL
+   */
   async paginatePosts(paginatePostDto: PaginatePostDto) {
     const posts = await this.postsRepository.find({
       where: {
@@ -39,18 +50,39 @@ export class PostsService {
       take: paginatePostDto.take,
     });
 
+    // 해당되는 포스트가 0개 이상이면 마지막 포스트를 가져오고
+    // 아니면 null을 반환
+    const lastItem = posts.length > 0 ? posts[posts.length - 1] : null;
+
+    const nextUrl = lastItem && new URL(`${PROTOCOL}://${HOST}/posts`);
+
     /**
-     * Response
-     *
-     * data: Data[],
-     * cursor: {
-     *  after: 마지막 data의 ID,
-     * },
-     * count: 응답한 data의 개수,
-     * next: 다음 요청을 할 때 사용할 URL
+     * DTO의 키값들을 순회하며
+     * 키값에 해당되는 밸류가 존재하면 param에 그대로 붙여준다.
+     * 단, where__id_more_than 값만 lastItem의 마지막 값으로 넣어준다.
      */
+    if (nextUrl) {
+      for (const key of Object.keys(paginatePostDto)) {
+        if (paginatePostDto[key]) {
+          if (key !== "where__id_more_than") {
+            nextUrl.searchParams.append(key, paginatePostDto[key]);
+          }
+        }
+      }
+
+      nextUrl.searchParams.append(
+        "where__id_more_than",
+        lastItem.id.toString(),
+      );
+    }
+
     return {
       data: posts,
+      cursor: {
+        after: lastItem?.id,
+      },
+      count: posts.length,
+      next: nextUrl?.toString(),
     };
   }
 
