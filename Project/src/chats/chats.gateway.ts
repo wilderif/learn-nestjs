e100aee89441ad3ca6c666b2379341173ccfcdeb
wilderif <woojuro3@gmail.com>
2024-10-18 12:12:59 +0900

    feat: Add ChatsGateway with message handling and connection logging - Implement WebSocketGateway with 'chats' namespace - Add 'send_message' event listener and connection logging

--- Project/src/chats/chats.gateway.ts
+++ Project/src/chats/chats.gateway.ts
@@ -1,1 +1,24 @@
-export class ChatsGateway {}
+import {
+  MessageBody,
+  OnGatewayConnection,
+  SubscribeMessage,
+  WebSocketGateway,
+} from "@nestjs/websockets";
+import { Socket } from "socket.io";
+
+@WebSocketGateway({
+  // ws://localhost:3000/chats
+  namespace: "chats",
+})
+export class ChatsGateway implements OnGatewayConnection {
+  handleConnection(socket: Socket) {
+    console.log(`on connection: ${socket.id}`);
+  }
+
+  // socket.on("send_message", (message) => { console.log(message); });
+  // 와 같은 역할
+  @SubscribeMessage("send_message")
+  sendMessage(@MessageBody() message: string) {
+    console.log(message);
+  }
+}
--- Project/src/chats/chats.module.ts
+++ Project/src/chats/chats.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { ChatsService } from './chats.service';
-import { ChatsController } from './chats.controller';
+import { Module } from "@nestjs/common";
+import { ChatsService } from "./chats.service";
+import { ChatsController } from "./chats.controller";
+import { ChatsGateway } from "./chats.gateway";
 
 @Module({
   controllers: [ChatsController],
-  providers: [ChatsService],
+  providers: [ChatsService, ChatsGateway],
 })
 export class ChatsModule {}
