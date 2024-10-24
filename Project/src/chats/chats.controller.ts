68c925dc4a0b86111e6d6b7dd2a4065690b18474
wilderif <woojuro3@gmail.com>
2024-10-24 12:48:17 +0900

    feat: Add paginateChats method in ChatsService using common paginate function
    - Added paginateChats method in ChatsService utilizing common module's paginate function
    - Use overideFindOptions to include user relations in the result
    - Added paginateChat endpoint to ChatsController's Get method, leveraging ChatsService's paginateChats

--- Project/src/chats/chats.controller.ts
+++ Project/src/chats/chats.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
-import { ChatsService } from './chats.service';
+import { Controller, Get, Query } from "@nestjs/common";
+import { ChatsService } from "./chats.service";
+import { PaginateChatDto } from "./dto/paginate-chat.dto";
 
-@Controller('chats')
+@Controller("chats")
 export class ChatsController {
   constructor(private readonly chatsService: ChatsService) {}
+
+  @Get()
+  paginateChat(@Query() paginateChatDto: PaginateChatDto) {
+    return this.chatsService.paginateChats(paginateChatDto);
+  }
 }
--- Project/src/chats/chats.service.ts
+++ Project/src/chats/chats.service.ts
@@ -3,13 +3,29 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { ChatsModel } from "./entity/chats.entity";
 import { CreateChatDto } from "./dto/create-chat.dto";
+import { CommonService } from "src/common/common.service";
+import { PaginateChatDto } from "./dto/paginate-chat.dto";
 
 @Injectable()
 export class ChatsService {
   constructor(
     @InjectRepository(ChatsModel)
     private readonly chatsRepository: Repository<ChatsModel>,
+    private readonly commonService: CommonService,
   ) {}
+
+  paginateChats(paginateChatDto: PaginateChatDto) {
+    return this.commonService.paginate(
+      paginateChatDto,
+      this.chatsRepository,
+      {
+        relations: {
+          users: true,
+        },
+      },
+      "chats",
+    );
+  }
 
   async createChat(createChatDto: CreateChatDto) {
     const chat = await this.chatsRepository.save({
